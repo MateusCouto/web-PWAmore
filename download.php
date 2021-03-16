62011cb9a31f8eebd78cc53f3268f335126716bb
Mateus Web <mateus.webjogos@gmail.com>
2021-03-16 02:01:44 -0300

    Web Source

---
+++ download.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="author" content="Perfect World Amore">
+<meta name="robots" content="index">
+<meta name="googlebot" content="index">
+<meta name="keywords" content="perfect world, 1.4.6, rate alta, full cubi, server brasileiro"/>
+<meta name="description" content="Perfect World Amore">
+<script type="text/javascript">
+</script>
+<link rel="shortcut icon" href="./favicon.ico">
+<title>Perfect World Amore</title>
+<script type="text/javascript" src="res/javascript.js" tppabs="/res/javascript.js"></script>
+<script type="text/javascript" src="res/power/power.js" tppabs="/res/power/power.js"></script>
+<script type="text/javascript">
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = '../apis.google.com/js/plusone.js'/*tpa=https://apis.google.com/js/plusone.js*/;
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
+<?php /* SLIDE */ ?>
+<script type="text/javascript" src="js/jquery.js" tppabs="/js/jquery.js"></script>
+<script type="text/javascript" src="js/customSelect.jquery.js" tppabs="/js/customSelect.jquery.js"></script>
+<script type="text/javascript" src="js/rhinoslider-1.05.min.js" tppabs="/js/rhinoslider-1.05.min.js"></script>
+<script type="text/javascript" src="js/mousewheel.js" tppabs="/js/mousewheel.js"></script>
+<script type="text/javascript" src="js/easing.js" tppabs="/js/easing.js"></script>
+<script type="text/javascript">
+		$(document).ready(function() {
+		$('#slideshow').rhinoslider({
+			effect: 'fade',
+			showTime: 5000,
+			controlsMousewheel: false,
+			controlsKeyboard: false,
+			controlsPrevNext: false,
+			controlsPlayPause: false,
+			autoPlay: true,
+			pauseOnHover: false,
+			showBullets: 'never',
+			changeBullets: 'before'
+		});
+		});
+		</script>
+<?php /* Facebook abaixo */ ?>		
+		<div id="fb-root"></div>
+		<script>(function(d, s, id) {
+				var js, fjs = d.getElementsByTagName(s)[0];
+				if (d.getElementById(id)) return;
+				js = d.createElement(s); js.id = id;
+				js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1";
+				fjs.parentNode.insertBefore(js, fjs);
+				}
+				(document, 'script', 'facebook-jssdk'));
+		</script>
+
+<link href="res/power/power.css" tppabs="/res/power/power.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" tppabs="/css/style.css" rel="stylesheet" type="text/css">
+<link href="css/slider.css" tppabs="/css/slider.css" rel="stylesheet" type="text/css">
+
+</head><body>
+<div align="center">
+<div id="container">
+<!--- MENU --->
+<div id="header">
+	<div class="menu">
+		<ul class="clearfix">
+			<li><a id="home" href="index.php"><p></p><span></span></a></li>
+			<li><a id="forum" href="http://forum.pwhits.net/" target="_blank"><p></p><span></span></a></li>
+			<li><a id="download" href="download.php"><p></p><span></span></a></li>
+			<li><a id="logo" href="index.php" tppabs="index.php"></a></li>
+			<li><a id="informacoes" href="#"><p></p><span></span></a></li>
+			<li><a id="equipe" href="#"><p></p><span></span></a></li>
+			<li><a id="suporte" href="http://pwhits.net/ticket/" target="_blank"><p></p><span></span></a></li>
+		</ul>
+<!--- LOGO --->
+		<ul class="logo-cont">
+			<li><a href="#" tppabs="#"></a></li>
+		</ul>
+	</div>
+</div>
+<br>
+<div id="content">
+	<table class="content" cellpadding="0" cellspacing="0">
+	<tbody>
+		<tr> <td id="page2">
+		<div class="post2">
+		<!--- TÍTULO --->
+		<div class="post_header2">Download</div>
+		<div class="post_body2" align="left"><center><div class='sub-box3' align='left'>
+		<!--- CONTEÚDO --->
+<center>
+<b><font color='#ffe62c'>Download Earth Atualizado 01.15.2013</font></b>
+</center><div class='line2'></div>
+<center>
+<p>Para jogar o PWHits Earth, você deve baixar e instalar o cliente do jogo.
+É gratuito para baixar e jogar, Gold Semanal 1600!</p>
+<br>
+
+</center>
+<div class='line2'></div><center><b><font color='#ffe62c'>2)Links Para Download:</font></b></center><div class='line2'></div>
+<center>
+<p>Para baixar por Torrent recomendos o Programa <a href='http://www.utorrent.com/intl/pt/utorrent-plus/index/frmrvh' target='_blank'>uTorrent</a></p><br>
+<table border='0'>
+<tr>
+<td><center><font color='#ffe62c'><b>Baixar por Torrent</b></font></center></td>
+<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+<td><center><font color='#ffe62c'><b>Baixar Pelo Site Perfect World</b></font></center></td>
+</tr>
+<tr>
+</tr>
+<tr>
+<td><center><a href='http://pwhits.net/PWEarthDinastiaBR01.01.2014.torrent' target='_blank'>Clique Aqui para baixar</a></center></td>
+<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+<td><center><p><a href='https://www.adrive.com/public/MytnR4/PWEarthDinastiaBR01.01.2014.7z' target='_blank'>Clique Aqui para baixar</a> </p></center></td>
+</tr>
+</table>
+<br><p>Utilize o programa <a href='http://www.7-zip.org/' target='_blank'>7zip</a>, <a href='http://www.rarlab.com/download.htm' target='_blank'>Winrar</a> (ou semelhantes) para descompactar. Depois de pegar nosso Cliente, leia as Regras!</p><br>
+</center>
+<div class='line2'></div><p><center><b><font color='#ffe62c'>3) LAUNCHER</font></b></center></p><div class='line2'></div>
+	<p>Caso você esteja com problema com o AutoUpdate ou erro, recomendamos colocar nosso Launcher e refazer a atualização! <a href='http://pwhits.net/LauncherEarthv3.0.exe' target='_blank'>Baixar Agora</a></p><br>
+		</div> </td>
+		</tr>
+	</tbody></table>
+		</div>
+<!--- FIM --->
+<div class="footer clearfix">
+		<div id="left">
+		Perfect World Amore - Servidor 1.4.6 / 100% Full-Cubi<br/>
+		Armaduras, Acessórios Totalmente Gratuitos!<br/>
+		Mais uma jogo do <a target="_blank" href="http://pwhits.com.br/" target="_blank">PW Hits!</a>
+		</div>
+	<div id="right">
+		<a href="index.php">home</a>
+		<a href="http://forum.pwhits.net/" target="_blank">fórum</a>
+		<a href="download.php">download</a>
+		<a href="#">informações</a>
+		<a href="#">equipe</a>
+		<a href="http://pwhits.net/ticket/" target="_blank">suporte</a>
+	</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</body></html>
---
+++ index.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="author" content="Perfect World Amore">
+<meta name="robots" content="index">
+<meta name="googlebot" content="index">
+<meta name="keywords" content="perfect world, 1.4.6, rate alta, full cubi, server brasileiro"/>
+<meta name="description" content="Perfect World Amore">
+<script type="text/javascript">
+</script>
+<link rel="shortcut icon" href="./favicon.ico">
+<title>Perfect World Amore</title>
+<script type="text/javascript" src="res/javascript.js" tppabs="/res/javascript.js"></script>
+<script type="text/javascript" src="res/power/power.js" tppabs="/res/power/power.js"></script>
+<script type="text/javascript">
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = '../apis.google.com/js/plusone.js'/*tpa=https://apis.google.com/js/plusone.js*/;
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
+<?php /* SLIDE */ ?>
+<script type="text/javascript" src="js/jquery.js" tppabs="/js/jquery.js"></script>
+<script type="text/javascript" src="js/customSelect.jquery.js" tppabs="/js/customSelect.jquery.js"></script>
+<script type="text/javascript" src="js/rhinoslider-1.05.min.js" tppabs="/js/rhinoslider-1.05.min.js"></script>
+<script type="text/javascript" src="js/mousewheel.js" tppabs="/js/mousewheel.js"></script>
+<script type="text/javascript" src="js/easing.js" tppabs="/js/easing.js"></script>
+<script type="text/javascript">
+		$(document).ready(function() {
+		$('#slideshow').rhinoslider({
+			effect: 'fade',
+			showTime: 5000,
+			controlsMousewheel: false,
+			controlsKeyboard: false,
+			controlsPrevNext: false,
+			controlsPlayPause: false,
+			autoPlay: true,
+			pauseOnHover: false,
+			showBullets: 'never',
+			changeBullets: 'before'
+		});
+		});
+		</script>
+<?php /* Facebook abaixo */ ?>		
+		<div id="fb-root"></div>
+		<script>(function(d, s, id) {
+				var js, fjs = d.getElementsByTagName(s)[0];
+				if (d.getElementById(id)) return;
+				js = d.createElement(s); js.id = id;
+				js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1";
+				fjs.parentNode.insertBefore(js, fjs);
+				}
+				(document, 'script', 'facebook-jssdk'));
+		</script>
+
+<link href="res/power/power.css" tppabs="/res/power/power.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" tppabs="/css/style.css" rel="stylesheet" type="text/css">
+<link href="css/slider.css" tppabs="/css/slider.css" rel="stylesheet" type="text/css">
+
+</head><body>
+<div align="center">
+<div id="container">
+<!--- MENU --->
+<div id="header">
+	<div class="menu">
+		<ul class="clearfix">
+			<li><a id="home" href="index.php"><p></p><span></span></a></li>
+			<li><a id="forum" href="http://forum.pwhits.net/" target="_blank"><p></p><span></span></a></li>
+			<li><a id="download" href="download.php"><p></p><span></span></a></li>
+			<li><a id="logo" href="index.php" tppabs="index.php"></a></li>
+			<li><a id="informacoes" href="#"><p></p><span></span></a></li>
+			<li><a id="equipe" href="#"><p></p><span></span></a></li>
+			<li><a id="suporte" href="http://pwhits.net/ticket/" target="_blank"><p></p><span></span></a></li>
+		</ul>
+<!--- LOGO --->
+		<ul class="logo-cont">
+			<li><a href="#" tppabs="#"></a></li>
+		</ul>
+	</div>
+</div>
+<br>
+<!--- PARTE CENTRAL --->
+<div id="content">
+	<table class="content" cellpadding="0" cellspacing="0">
+	<tbody>
+		<tr>
+		<td>
+<!--- SLIDE + LINKS IMPORTANTES --->
+<div>
+	<div class="slider box-shadow">
+	<ul id="slideshow">
+		<li><img src="res/slider/slide-1.jpg" tppabs="/res/slider/slide-1.jpg" alt=""/></li>
+		<li><img src="res/slider/slide-2.jpg" tppabs="/res/slider/slide-2.jpg" alt=""/></li>
+		<li><img src="res/slider/slide-3.jpg" tppabs="/res/slider/slide-3.jpg" alt=""/></li>
+		<li><img src="res/slider/slide-4.jpg" tppabs="/res/slider/slide-4.jpg" alt=""/></li>
+		<li><img src="res/slider/slide-5.jpg" tppabs="/res/slider/slide-5.jpg" alt=""/></li>
+	</ul>
+	</div>
+	<div class="quick-menu-container box-shadow">
+	<ul class="clearfix">
+		<li id="vote">
+			<a href="#"></a>
+		</li>
+		<li id="shop">
+			<a href="#"></a>
+		</li>
+		<li id="forum">
+			<a href="http://forum.pwhits.net/" target='_blank'></a>
+		</li>
+	</ul>
+	</div>
+</div><div class="post"></div>
+
+<!--- NOTÍCIAS AQUI --->
+<div class="post bordas-noticias">
+	<div class="post_header clearfix">
+		<div id="title"><a>Notícia Desativada!</a></div>
+		<div id="admin"></div>
+	</div>
+	<div class="post_body_news">
+	<p>	
+		<center><img src="http://i.imgur.com/fHiOZ.png" alt="Evento Amore!"> </center><br>
+		A PW Hits com colaboração da equipe do Perfect World Amore está promovendo um Evento! Clique em "Leia Mais" e participe deste evento imperdível! <b>Mas corra pois será até dia 30/01/2013!</b>
+	</p><hr/>
+		<div id="info" class="clearfix text-shadow">
+		<p id="post"></p>
+			<div style="width:300px; text-align: right; display: inline-block; float:right; width:50%">
+			 <a href="http://pwhits.com.br/v4/">leia mais!</a> 
+			</div>
+		</div>
+	</div>
+</div>
+<!--- NOTÍCIAS AQUI --->
+<div class="post bordas-noticias">
+	<div class="post_header clearfix">
+		<div id="title"><a>Notícia Desativada!</a></div>
+		<div id="admin"></div>
+	</div>
+	<div class="post_body_news">
+	<p>	
+		Notícia Desativada
+	</p><hr/>
+		<div id="info" class="clearfix text-shadow">
+		<p id="post"></p>
+			<div style="width:300px; text-align: right; display: inline-block; float:right; width:50%">
+			 <a href="http://pwhits.com.br/v4/">leia mais!</a> 
+			</div>
+		</div>
+	</div>
+</div>
+<!--- NOTÍCIAS AQUI --->
+<div class="post bordas-noticias">
+	<div class="post_header clearfix">
+		<div id="title"><a>Notícia Desativada!</a></div>
+		<div id="admin"></div>
+	</div>
+	<div class="post_body_news">
+	<p>	
+		Notícia Desativada
+	</p><hr/>
+		<div id="info" class="clearfix text-shadow">
+		<p id="post"></p>
+			<div style="width:300px; text-align: right; display: inline-block; float:right; width:50%">
+			 <a href="http://pwhits.com.br/v4/">leia mais!</a> 
+			</div>
+		</div>
+	</div>
+</div>
+<!--- NOTÍCIAS AQUI --->
+<div class="post bordas-noticias">
+	<div class="post_header clearfix">
+		<div id="title"><a>Notícia Desativada!</a></div>
+		<div id="admin"></div>
+	</div>
+	<div class="post_body_news hovertexto">
+	<p>	
+		Notícia Desativada
+	</p><hr/>
+		<div id="info" class="clearfix text-style>
+		<p id="post"></p>
+			<div style="width:300px; text-align: right; display: inline-block; float:right; width:50%">
+			 <a href="http://pwhits.com.br/v4/">leia mais!</a> 
+			</div>
+		</div>
+	</div>
+</div>
+
+	<!--- Estatísticas --->
+		</td>
+			<td class="sidebar">
+				<div class="sidebar-container">
+					<a class="banner box-shadow" id="registrar" href="#" target="_blank"></a>
+				</div>
+				<div class="sidebar-container">
+					<a class="banner box-shadow" id="painel" href="#" target="_blank"></a>
+				</div>
+	<!--- Estatísticas --->
+<div class="side-r">
+<div class="sidebar-container estatisticas2 bordas-noticias box text-style">
+<div class="mems-b-head">
+<p id="login-text"></p>
+</div>
+	<div class="estatisticas1">
+amore 1.4.6:  online!<br>
+players(s): 34.654
+	</div>
+</div>
+
+	<!--- TeamSpeak --->
+	<div class="sidebar-container">
+	<a class="banner box-shadow" id="teamspeak" href="#"></a>
+	</div>
+	<!--- Donate --->
+	<div class="sidebar-container">
+	<a class="banner box-shadow" id="donate" href="#"></a>
+	</div>
+<!--- FACEBOOK --->
+	<div class="facebookparagrafo">
+	<center>
+		<iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FPWHitsGames&amp;width=294&amp;height=290&amp;show_faces=true&amp;colorscheme=light&amp;stream=false&amp;border_color=%2386254e&amp;header=true" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:294px; height:290px;" allowTransparency="true"></iframe>
+	</center>
+	</div>
+			</td></tr>
+	</tbody></table>
+	</div>
+<!--- FIM --->
+<div class="footer clearfix">
+		<div id="left">
+		Perfect World Amore - Servidor 1.4.6 / 100% Full-Cubi<br/>
+		Armaduras, Acessórios Totalmente Gratuitos!<br/>
+		Mais uma jogo do <a target="_blank" href="http://pwhits.com.br/" target="_blank">PW Hits!</a>
+		</div>
+	<div id="right">
+		<a href="index.php">home</a>
+		<a href="http://forum.pwhits.net/" target="_blank">fórum</a>
+		<a href="download.php">download</a>
+		<a href="#">informações</a>
+		<a href="#">equipe</a>
+		<a href="http://pwhits.net/ticket/" target="_blank">suporte</a>
+	</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</body></html>
